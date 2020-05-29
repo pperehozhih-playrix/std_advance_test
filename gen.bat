3a1a3299bfbebcc8e5d53104cfc2878fc8ca2c3d
Pavel Perekhozhikh <p.perehogih@gmail.com>
2020-05-29 10:44:36 +0300

    Initial commit

---
+++ gen.bat
@@ -0,0 +1,4 @@
+mkdir build
+pushd build
+cmake .. -T LLVM_v141
+popd
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iterator>
+#include <iterator>
+#include <vector>
+
+int main() {
+	std::vector<int> v{ 3, 1, 4 };
+	{
+		auto foo = v.begin();
+		long bar = 1;
+		std::advance(foo, bar);
+	}
+	{
+		auto foo = v.begin();
+		size_t bar = 1;
+		std::advance(foo, bar);
+	}
+	return 0;
+}
